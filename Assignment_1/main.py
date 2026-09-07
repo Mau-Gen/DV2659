202804060e32e8f7a92eb3df9adae19d82e204bb
MaurasBraj <mauritz.almgren@gmail.com>
2026-09-07 15:00:15 +0200

    Developed the utility agent to find the best satisfaction combination of a shopping trip.

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -1,8 +1,20 @@
+from itertools import combinations
+
 class Item():
     def __init__(self, n: str, p: int, s: int):
         self.name = n
         self.price = p
         self.satisfaction = s
+    def __repr__(self):
+        return "[{},{},{}]".format(self.name, str(self.price), str(self.satisfaction))
+
+class Combination():
+    def __init__(self, l: list, p: int, s: int):
+        self.list = l
+        self.total_price = p
+        self.total_satisfaction = s
+    def __repr__(self):
+        return "[{},{},{}]".format(self.list, str(self.total_price), str(self.total_satisfaction))
 
 def reflex_agent(budget, items):
     basket = []
@@ -24,16 +36,26 @@ def reflex_agent(budget, items):
         print(f"{item.name, item.price, item.satisfaction}\n")
 
     print(f"The total price of the basket is {amount}.\n")
-    print(f"The total satisfaction of the basket is {total_s}.")
+    print(f"The total satisfaction of the basket is {total_s}.\n")
 
 def utility_agent(budget, items):
-    basket = []
-    amount = 0
-    total_s = 0
+    combs = []
+
+    for i in range(1, len(items)+1):
+        for r in combinations(items, i):
+            comb_price = sum(item.price for item in r)
+            comb_satisfaction = sum(item.satisfaction for item in r)
+            if comb_price <= budget:
+                combs.append(Combination(r, comb_price, comb_satisfaction))
+    print(f"These are all the combinations that are possible for the budget:\n\n{combs}\n")
+    best_comb = max(combs, key=lambda comb: (comb.total_satisfaction, -comb.total_price))
+
+    return print(f"This is the best possible combination for satisfaction:\n{best_comb} \nThe total satisfaction is: {best_comb.total_satisfaction}")
 
 def main():
     items = [Item("Bread", 5, 6), Item("Milk", 4, 7), Item("Eggs", 6, 8), Item("Chocolate", 8, 9)]
     reflex_agent(20, items)
+    utility_agent(20, items)
 
 if __name__ == "__main__":
     main()
